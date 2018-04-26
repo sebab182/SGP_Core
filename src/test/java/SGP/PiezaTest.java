4966328f895bcad2cb54e9c3e1144fdb1b6e76db
Nicolás Cancela <nicolasecancela@gmail.com>
2018-04-26 13:27:17 -0300

    Aumento de cobertura Pieza Test

--- src/test/java/SGP/PiezaTest.java
+++ src/test/java/SGP/PiezaTest.java
@@ -5,24 +5,35 @@ import java.util.Date;
 import junit.framework.TestCase;
 
 public class PiezaTest extends TestCase {
-	private Pieza p;
+	Pieza p;
 	
-	@SuppressWarnings("deprecation")
 	public PiezaTest() {
 		p = new Pieza(new Tipo("muslo"),new Date(118,1,21));
 	}
+	
+	public void testHashCode() {
+		Pieza a = new Pieza(new Tipo("muslo"),new Date());
+		assertNotSame(a.hashCode(),p.hashCode());
+	}
+
+	public void testGetTipoPieza() {
+		assertEquals(p.getTipoPieza(),new Tipo("muslo"));
+	}
+
+	public void testGetFechaVencimiento() {
+		assertNotSame(p.getFechaVencimiento(),new Date());
+	}
 
 	public void testEqualsObject() {
-		Pieza aux = new Pieza(new Tipo("muslo"),new Date());
-		assertNotSame(aux,p);
+		Pieza a = new Pieza(new Tipo("muslo"),new Date());
+		assertNotSame(a,p);
+		
+		Pieza b = new Pieza(new Tipo("vacio"),null);
+		assertNotSame(b,p);
 	}
 
 	public void testToString() {
 		assertEquals("muslo",p.toString());
-	}
-	
-	public void testGetFechaVencimiento() {
-		assertNotSame(p.getFechaVencimiento(),new Date());
 	}
 
 }
