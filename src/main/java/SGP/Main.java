e2d12ff5e4cd246cbe514bb1cec56535f7257bea
Sebastian Fretes <37638724+sebab182@users.noreply.github.com>
2018-04-26 21:41:37 -0300

    prueba exclude
    prueba exclude

--- src/main/java/SGP/Main.java
+++ src/main/java/SGP/Main.java
@@ -10,17 +10,17 @@ public class Main {
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		//DataSource selecciona el tipo de factory a cargar
-		//DataSource ds = new DataSource("datasource.txt");
-		//Class cls = Class.forName(ds.getFactory());
-		//AbstractFactory af = (AbstractFactory) cls.newInstance();
+		DataSource ds = new DataSource("datasource.txt");
+		Class cls = Class.forName(ds.getFactory());
+		AbstractFactory af = (AbstractFactory) cls.newInstance();
 		
 		//Cargamos stock y pedidos
-		//GestordeStock gestorStock = af.cargarGestordeStock();
-		//System.out.println(gestorStock.getStock());
-		//List<Pedido<Tipo>>pedidos=af.cargarPedidos();
+		GestordeStock gestorStock = af.cargarGestordeStock();
+		System.out.println(gestorStock.getStock());
+		List<Pedido<Tipo>>pedidos=af.cargarPedidos();
 		
 		//Creamos distribuidor y resolvemos los pedidos
-		//Distribuidor d = new Distribuidor();
+		Distribuidor d = new Distribuidor();
 		//List<Map<Pieza,Integer>>distribuciones = d.resolverPedidos(pedidos, gestorStock);
 	}
 }
